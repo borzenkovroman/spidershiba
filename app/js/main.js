db947edc10a443a66794c245f9b04a659c538bae
borzenkovroman <borzoman99@gmail.com>
2022-11-30 17:33:20 +0200

    added sections hero, about, play

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-
+  function myFunction() {
+    var copyText = document.getElementById("contract");
+    copyText.select();
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+  }
 });
